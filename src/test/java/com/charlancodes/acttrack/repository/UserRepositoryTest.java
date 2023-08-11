e63fe51f12f80a6133e1b32d8f7e301b97801235
Juliet Adiela <juliet.adiela@decagon.dev>
2023-08-11 16:23:30 +0100

    activity tracker completed

---
+++ src/test/java/com/charlancodes/acttrack/repository/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.charlancodes.acttrack.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.DERBY)
+public class UserRepositoryTest {
+    @Autowired
+    private  UserRepository userRepository;
+
+}
